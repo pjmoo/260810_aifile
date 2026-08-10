ba2729b56ed7d01fa97cacbd31579c90637e417e
pjmoo <00pjmoooo@gmail.com>
2026-08-10 10:45:24 +0900

    feat: PDF 업로드 핸들러 구현

--- src/main/java/org/example/aifile/controller/PDFController.java
+++ src/main/java/org/example/aifile/controller/PDFController.java
@@ -3,7 +3,11 @@ package org.example.aifile.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/pdf")
@@ -12,5 +16,15 @@ public class PDFController {
     @GetMapping
     public String formPage() {
         return "pdf/form";
+    }
+
+    @PostMapping
+    public String uploadPDF(@RequestParam MultipartFile file,
+                            RedirectAttributes redirectAttributes) {
+        System.out.println("file = " + file);
+        System.out.println(file.getContentType());
+        System.out.println(file.getOriginalFilename());
+        redirectAttributes.addFlashAttribute("msg", "파일 업로드 완료");
+        return "redirect:/pdf";
     }
 }
