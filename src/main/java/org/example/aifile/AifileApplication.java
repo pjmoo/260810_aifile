62c2e66f180d7b013d073aeebad5ced2b5df79ac
pjmoo <00pjmoooo@gmail.com>
2026-08-10 09:45:12 +0900

    Initial commit

---
+++ src/main/java/org/example/aifile/AifileApplication.java
@@ -0,0 +1,13 @@
+package org.example.aifile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AifileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AifileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aifile/AifileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aifile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AifileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
